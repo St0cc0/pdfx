b58068377ff26f91a6388afdef0e5f7fd9e7dd57
Joshua Stocco <joshua.stocco@frontiersin.org>
2018-10-10 17:12:55 +0200

    Updated downloader.py
    Updated to use requests module and to mark all 200 and 300 codes as ok.

--- pdfx/downloader.py
+++ pdfx/downloader.py
@@ -5,20 +5,13 @@ import os
 import sys
 IS_PY2 = sys.version_info < (3, 0)
 
-if IS_PY2:
-    # Python 2
-    from urllib2 import Request, urlopen, HTTPError, URLError
-else:
-    # Python 3
-    from urllib.request import Request, urlopen, HTTPError, URLError
-    unicode = str
-
 import ssl
 
 from collections import defaultdict
 from .threadpool import ThreadPool
 from .colorprint import colorprint, OKGREEN, FAIL
 
+header = {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"}
 MAX_THREADS_DEFAULT = 7
 
 # Used to allow downloading files even if https certificate doesn't match
@@ -31,26 +24,26 @@ else:
 
 def sanitize_url(url):
     """ Make sure this url works with urllib2 (ascii, http, etc) """
-    if url and not url.startswith("http"):
+    if url and url.startswith("10."): # condition added to convert DOI to url
+        url = u"http://doi.org/%s" % url
+    elif url and not url.startswith("http"):
         url = u"http://%s" % url
-    url = url.encode('ascii', 'ignore').decode("utf-8")
     return url
 
 
 def get_status_code(url):
     """ Perform HEAD request and return status code """
     try:
-        request = Request(sanitize_url(url))
-        request.add_header("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; "
-                           "Windows NT 6.1; Trident/5.0)")
-        request.get_method = lambda: 'HEAD'
-        response = urlopen(request, context=ssl_unverified_context)
-        # print response.info()
-        return response.getcode()
-    except HTTPError as e:
-        return e.code
-    except URLError as e:
-        return e.reason
+        request = requests.get(sanitize_url(url), headers = header)
+        return request.status_code
+    except requests.HTTPError as e:
+        return "HTTP Error"
+    except requests.URLRequired as e:
+        return "URL Error"
+    except requests.Timeout as e:
+        return "Timeout Error"
+    except requests.ConnectionError as e:
+        return "Connection Error"
     except Exception as e:
         print(e, url)
         return None
@@ -59,13 +52,14 @@ def get_status_code(url):
 def check_refs(refs, verbose=True, max_threads=MAX_THREADS_DEFAULT):
     """ Check if urls exist """
     codes = defaultdict(list)
+    numberworking = 0 #counter for the number of working links
 
     def check_url(ref):
         url = ref.ref
         status_code = str(get_status_code(url))
         codes[status_code].append(ref)
         if verbose:
-            if status_code == "200":
+            if status_code.startswith("2") or status_code.startswith("3"): # added condition to make all 200 and 300 codes ok
                 colorprint(OKGREEN, "%s - %s" % (status_code, url))
             else:
                 colorprint(FAIL, "%s - %s" % (status_code, url))
@@ -83,16 +77,17 @@ def check_refs(refs, verbose=True, max_threads=MAX_THREADS_DEFAULT):
 
     # Print summary
     print("\nSummary of link checker:")
-    if "200" in codes:
-        colorprint(OKGREEN, "%s working" % len(codes["200"]))
     for c in sorted(codes):
-        if c != "200":
+        if c.startswith("2") or c.startswith("3"): # added condition to make all 200 and 300 codes count as being ok
+            numberworking += len(codes[c])
+        else:
             colorprint(FAIL, "%s broken (reason: %s)" % (len(codes[c]), c))
             for ref in codes[c]:
                 o = u"  - %s" % ref.ref
                 if ref.page > 0:
                     o += " (page %s)" % ref.page
                 print(o)
+    colorprint(OKGREEN, "%s working" % numberworking)
 
 
 def download_urls(urls, output_directory, verbose=True,
@@ -109,23 +104,24 @@ def download_urls(urls, output_directory, verbose=True,
     def download_url(url):
         try:
             fn = url.split("/")[-1]
-            fn_download = os.path.join(output_directory, fn)
+            fn_download = os.path.join(output_directory, str(url))
             with open(fn_download, "wb") as f:
-                request = Request(sanitize_url(url))
-                request.add_header("User-Agent", "Mozilla/5.0 (compatible; "
-                                   "MSIE 9.0; Windows NT 6.1; Trident/5.0)")
-                response = urlopen(request, context=ssl_unverified_context)
-                status_code = response.getcode()
+                request = requests.get(sanitize_url(url), headers = header)
+                status_code = request.status_code
                 if status_code == 200:
-                    f.write(urlopen(request).read())
+                    f.write(request.read())
+                    colorprint(OKGREEN, "Downloaded '%s' to '%s'" %
+                                        (url, fn_download))
+                elif status_code == 302:
+                    f.write(request.read())
                     colorprint(OKGREEN, "Downloaded '%s' to '%s'" %
                                         (url, fn_download))
                 else:
                     colorprint(FAIL, "Error downloading '%s' (%s)" %
                                      (url, status_code))
-        except HTTPError as e:
+        except requests.HTTPError as e:
             colorprint(FAIL, "Error downloading '%s' (%s)" % (url, e.code))
-        except URLError as e:
+        except requests.URLRequired as e:
             colorprint(FAIL, "Error downloading '%s' (%s)" % (url, e.reason))
         except Exception as e:
             colorprint(FAIL, "Error downloading '%s' (%s)" % (url, str(e)))
